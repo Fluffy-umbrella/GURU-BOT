7df85ba998db84a4bbc48e0b9a02cae384800c14
Fluffy-umbrella <155983710+Fluffy-umbrella@users.noreply.github.com>
2024-01-08 17:59:31 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['254757889704', 'Mr-NoBoDYy', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
